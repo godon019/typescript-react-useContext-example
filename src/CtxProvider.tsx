1ad31398be8a8b39aafc92361049451a4e3cf3a4
Dong-Kyun Ko <godon019@gmail.com>
2019-06-04 21:38:21 -0400

    use useState() hook

--- src/CtxProvider.tsx
+++ src/CtxProvider.tsx
@@ -8,30 +8,18 @@ interface AppContextInterface {
 
 export const SimpleCtx = React.createContext<AppContextInterface | null>(null);
 
-class CtxProvider extends React.Component<{}, AppContextInterface> {
-  setValue: SetValue;
-  constructor(props) {
-    super(props);
-
-    this.setValue = value => {
-      this.setState({
-        value
-      });
-    };
-
-    this.state = {
-      value: null,
-      setValue: this.setValue
-    };
-  }
-
-  render() {
-    return (
-      <SimpleCtx.Provider value={this.state}>
-        {this.props.children}
-      </SimpleCtx.Provider>
-    );
-  }
-}
+const CtxProvider: React.FC = props => {
+  const [value, setValue] = React.useState(null);
+  return (
+    <SimpleCtx.Provider
+      value={{
+        value: value,
+        setValue: setValue
+      }}
+    >
+      {props.children}
+    </SimpleCtx.Provider>
+  );
+};
 
 export default CtxProvider;
