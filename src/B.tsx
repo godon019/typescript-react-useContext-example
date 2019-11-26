a61df42bc1c327f2c97ffdaa36efe127c8292089
Dong-Kyun Ko <godon019@gmail.com>
2019-11-26 17:17:01 +0900

    Test commit
    text changes B->C

--- src/B.tsx
+++ src/B.tsx
@@ -7,7 +7,7 @@ const CtxConsumer = () => {
   const { value, setValue } = useContext(SimpleCtx);
   return (
     <div style={bStyle}>
-      <h2>Component B</h2>
+      <h2>Component C</h2>
       <div>value: {value}</div>
       <input value={value} onChange={e => setValue(e.target.value)} />
     </div>
